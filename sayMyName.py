7536254fe592e76d1b074c6925ba0ff8df0e8a96
joshunov <joshnov7@gmail.com>
2021-08-17 13:49:22 +1000

    Create sayMyName.py

---
+++ sayMyName.py
@@ -0,0 +1,19 @@
+'''
+## Proof of work, session 1
+Write a program called `sayMyName.py` that prints out your name N times, where N is the first digit of your SID. 
+For example, if your SID is 432422232 and your name is Kimi Nonawa, then you would print:
+```
+Kimi Nonawa
+Kimi Nonawa
+Kimi Nonawa
+... (repeat 432 times total)
+Kimi Nonawa 
+```
+'''
+
+
+my_name = "Josh Novick"
+SID = '510448983'
+N = int(SID[0:2])
+for i in range(N):
+    print(my_name)
